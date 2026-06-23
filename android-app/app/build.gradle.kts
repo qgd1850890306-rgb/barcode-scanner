a933341ca5bfd5d4eaf2f74346cd556e4889731b
qgd1850890306-rgb <qgd1850890306@gmail.com>
2026-06-23 17:40:02 +0800

    add Room + KSP deps

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
     }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
+    dexOptions { javaMaxHeapSize = "4g" }
     buildFeatures { compose = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.5" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
