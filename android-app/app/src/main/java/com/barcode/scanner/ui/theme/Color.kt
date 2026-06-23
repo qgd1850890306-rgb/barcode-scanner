ede1130346f0b41582b0b439d09f857a5a7c79a9
qgd1850890306-rgb <qgd1850890306@gmail.com>
2026-06-23 08:35:14 +0800

    添加颜色常量

---
+++ android-app/app/src/main/java/com/barcode/scanner/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+﻿package com.barcode.scanner.ui
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF667EEA)
+val Danger = Color(0xFFE53935)
+val Success = Color(0xFF43A047)
+val Background = Color(0xFFF5F6FA)
+val TextPrimary = Color(0xFF1A1A2E)
+val TextSecondary = Color(0xFF888888)
+val BadgeQR = Color(0xFFE3F2FD)
+val BadgeQRText = Color(0xFF1565C0)
+val BadgeDM = Color(0xFFFFF3E0)
+val BadgeDMText = Color(0xFFE65100)
