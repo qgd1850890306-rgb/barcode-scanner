4dbae3f070019ca56fd2143a074f5a0782670658
qgd1850890306-rgb <qgd1850890306@gmail.com>
2026-06-23 17:29:21 +0800

    add Room + KSP deps

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material:material-icons-core")
 
     // CameraX
     implementation("androidx.camera:camera-core:1.3.1")
